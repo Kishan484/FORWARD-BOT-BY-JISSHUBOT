51a1e14126b775c94ec314d5eaca181aaeacad91
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2025-03-28 19:07:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = environ.get("API_ID", "28103752")
     API_HASH = environ.get("API_HASH", "479579601469809b9a9c8ffc15fbe11a")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7945690681:AAHjvge7eGkJ-oIZeI-igCFzjuEmRDgDtfk") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7159500227').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
